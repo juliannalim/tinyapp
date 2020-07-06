7967f8167a59f84950c0eb9822f4bc72a5b6248f
Julianna <julianna.j.lim@gmail.com>
2020-07-06 19:14:30 -0400

    added rout for urls

--- express_server.js
+++ express_server.js
@@ -4,6 +4,7 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -19,4 +20,9 @@ app.get("/hello", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
