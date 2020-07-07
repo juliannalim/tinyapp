a3a3ca67f831076f278c3ce8db10d27a167f93cc
Julianna <julianna.j.lim@gmail.com>
2020-07-07 15:21:45 -0400

    app.post to delete

--- express_server.js
+++ express_server.js
@@ -59,3 +59,8 @@ app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[shortURL]
+  res.redirect('/urls/');
+});
