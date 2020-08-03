d41be0ba9c4e41519bffe5081561b2ed3be6fa54
Julianna <julianna.j.lim@gmail.com>
2020-08-03 16:52:41 -0400

    redirect once the link is deleted

--- express_server.js
+++ express_server.js
@@ -67,3 +67,10 @@ app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
+
+// REDIRECT WHEN DELETED //
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+});
