c0b175e0cc68db7252b918d0802fd1daeb325ab5
Julianna <julianna.j.lim@gmail.com>
2020-08-03 12:42:37 -0400

    to get and post the new url and send ok then added function to generate random string

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -32,8 +35,24 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+// NEW URL //
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+
 // SHORT URL //
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+
+function generateRandomString() {
+  return Math.random().toString(36).substring(7);
+}
